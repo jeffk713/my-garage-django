641524238c7ea6171867b21142d2b4378b2317c4
jeff <jeff.kim713@gmail.com>
2022-02-22 01:46:58 -0800

    add secret_key in .env

--- server/garage_backend/settings.py
+++ server/garage_backend/settings.py
@@ -25,7 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-uk!^o-*1_#z804a63xm^0u69o&-q-ik0ey(ibzvjfz(&!ydet='
+if not env_vars.get("SECRET_KEY"):
+    raise Exception("App SECRET_KEY is not set! Please add it to your .env file")
+SECRET_KEY = env_vars.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = not env_vars.get("ENV") == "production"
