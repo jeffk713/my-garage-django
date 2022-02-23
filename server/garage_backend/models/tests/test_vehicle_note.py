7bbede2b7bfd6865bd790e6ec6e3a5c002db48f6
jeff <jeff.kim713@gmail.com>
2022-02-23 10:15:06 -0800

    add vehicle note tests

--- server/garage_backend/models/tests/test_vehicle_note.py
+++ server/garage_backend/models/tests/test_vehicle_note.py
@@ -2,6 +2,7 @@ from django.utils import timezone
 
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.db import IntegrityError
 
 from garage_backend.models.vehicle_note import VehicleNote
 from garage_backend.models.vehicle import Vehicle
@@ -45,18 +46,24 @@ class VehicleNoteModelTests(TestCase):
         self.assertEqual(vehicle_note.note, 'test note')
         self.assertEqual(vehicle_note.vehicle, self.vehicle)
         
-    # def test_create_vehicle_note(self):
-    #     vehicle_note = VehicleNote.objects.create(
-    #         nextAppointment = models.DateTimeField(null=True, auto_now=False, auto_now_add=False),
-    #         lfTire=8,
-    #         rfTire=8,
-    #         lrTire=7.5,
-    #         rrTire=7.5,
-    #         fBrake=11,
-    #         rBrake=8,
-    #         note='test note',
-    #         vehicle = self.vehicle,
-    #     )
+    def test_create_vehicle_note_using_with_no_measurements(self):
+        """  Test, create vehicle note with no measurements raises no error """
         
-    #     self.assertEqual(vehicle_note.note, 'test note')
-    #     self.assertEqual(vehicle_note.vehicle, self.vehicle)
+        vehicle_note = VehicleNote.objects.create(
+            note='test note',
+            vehicle = self.vehicle,
+        )
+        
+        self.assertEqual(vehicle_note.nextAppointment, None)
+        self.assertEqual(vehicle_note.rfTire, None)
+        self.assertEqual(vehicle_note.fBrake, None)
+        self.assertEqual(vehicle_note.vehicle, self.vehicle)
+        
+    def test_create_vehicle_note_using_with_invalid_vehicle(self):
+        """  Test, create vehicle note with invalid vehicle raises error """
+        
+        with self.assertRaises(IntegrityError):
+            vehicle_note = VehicleNote.objects.create(
+                note='test note',
+                vehicle = None,
+            )
