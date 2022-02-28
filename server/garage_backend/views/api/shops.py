b9b9767f6c00488505dfbac3712a40b64b88b93c
jeff <jeff.kim713@gmail.com>
2022-02-27 19:34:29 -0800

    create shop post route

--- server/garage_backend/views/api/shops.py
+++ server/garage_backend/views/api/shops.py
@@ -8,7 +8,24 @@ from garage_backend.views.view_utils import exception_utils, object_utils
 
 
 class Shop(APIView):
-    pass
+    """
+    Create a shop
+    """
+    
+    def post(self, request, format=None):
+        try:
+            serializer = serializers.ShopSerializer(data=request.data)
+
+            if serializer.is_valid(raise_exception=True): 
+                serializer.save()
+
+            return JsonResponse(serializer.data, status=201)
+
+        except Exception as e:
+            print(e)
+            if err := serializer.errors:
+                return JsonResponse({"error": exception_utils.get_error_message_list(err)}, status=400)
+            return JsonResponse({"error": ["Server error has occurred"]}, status=500)
 
 
 class ShopDetail(APIView):
