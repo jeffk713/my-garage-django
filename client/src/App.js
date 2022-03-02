32d2e764dcea8db947ed247c8d9b6aa9921200eb
jeff <jeff.kim713@gmail.com>
2022-03-01 22:06:27 -0800

    create frame for app

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
+// import NavBar from './components/NavBar/NavBar';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App bg-lime-500">
-      APP START
+    <div className='App w-screen flex flex-row xl:w-10/12 h-screen mx-auto'>
+      <div className='bg-sky-600 grow-0 w-52 md:w-1/5 lg:w-1/4'>
+        <div>LINK1</div>
+        <div>LINK2</div>
+        <div>LINK3</div>
+      </div>
+      <div className='bg-slate-200 grow text-gray-800'>APP START</div>
     </div>
   );
 }
---
+++ client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div className='bg-lime-500 grow'>
+            <div>LINK1</div>
+            <div>LINK2</div>
+            <div>LINK3</div>
+        </div>
+    );
+};
+
+export default NavBar;
