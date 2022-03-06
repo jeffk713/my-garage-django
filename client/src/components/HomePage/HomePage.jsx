39671cb6a68f6512e1dbf184a31f0cb639fd1376
jeff <jeff.kim713@gmail.com>
2022-03-05 17:29:36 -0800

    style homepage

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -1,9 +1,14 @@
 const HomePage = () => {
   return (
-    <div className='w-screen flex flex-col justify-center items-center'>
-      <h2 className=''>Manage Vehicles At Your Click</h2>
-      <span>
-        Not registered yet? Click <code>here</code>
+    <div className='w-screen h-3/4 flex flex-col justify-center items-center text-slate-100'>
+      <div className=' bg-zinc-700 py-4'>
+        <p className='w-screen text-center text-4xl font-bold'>
+          Manage Vehicles At Your Click
+        </p>
+      </div>
+      <span className='text-slate-100'>
+        Not registered yet? Click{' '}
+        <code className='font-bold cursor-pointer'>HERE</code>
       </span>
     </div>
   );
