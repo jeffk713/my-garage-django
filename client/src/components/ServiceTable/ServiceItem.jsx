83b6fc00856c6ddfaf7882a213ac9db4fcbf027d
jeff <jeff.kim713@gmail.com>
2022-03-12 02:33:25 -0800

    service table style updated

--- client/src/components/ServiceTable/ServiceItem.jsx
+++ client/src/components/ServiceTable/ServiceItem.jsx
@@ -1,7 +1,11 @@
-const ServiceItem = ({ ...service }) => {
+const ServiceItem = ({ index, ...service }) => {
   const { name, mileage, serviceDate, isWarranty } = service;
   return (
-    <div className='w-full grid grid-cols-6 gap-2 items-stretch text-center py-2'>
+    <div
+      className={`w-full grid grid-cols-6 gap-2 items-stretch text-center py-1 mt-0.5 ${
+        index % 2 ? 'bg-neutral-600' : 'bg-neutral-500'
+      }`}
+    >
       <div className='col-span-2 overflow-hidden'>{name}</div>
       <div>{mileage}</div>
       <div>{isWarranty ? 'Yes' : 'No'}</div>
--- client/src/components/ServiceTable/ServiceTable.jsx
+++ client/src/components/ServiceTable/ServiceTable.jsx
@@ -3,14 +3,14 @@ import { ServiceItem } from '.';
 const ServiceTable = ({ services }) => {
   return (
     <div className='font-semibold flex flex-col items-center w-full pt-2 mb-4'>
-      <div className='w-full bg-amber-600 grid grid-cols-6 gap-2 items-stretch text-center'>
+      <div className='w-full bg-amber-600 grid grid-cols-6 gap-2  py-1 items-stretch text-center'>
         <div className='col-span-2'>Service</div>
         <div>Mileage</div>
         <div>Warranty</div>
         <div className='col-span-2'>Date</div>
       </div>
-      {services.map(service => (
-        <ServiceItem key={service.id} {...service} />
+      {services.map((service, index) => (
+        <ServiceItem key={service.id} {...service} index={index}/>
       ))}
     </div>
   );
