4014d429d97a8402209b724ff468a9257cc69344
jeff <jeff.kim713@gmail.com>
2022-04-24 01:56:45 -0700

    implement service popup background

--- client/src/components/Pages/VehicleInfo/ServicePopup/ServicePopup.jsx
+++ client/src/components/Pages/VehicleInfo/ServicePopup/ServicePopup.jsx
@@ -18,6 +18,9 @@ const ServicePopup = ({
 }) => {
   const params = useParams();
   const isExistent = serviceToDisplay.id;
+  if (!serviceToDisplay.note) {
+    serviceToDisplay.note = '';
+  }
 
   const INITIAL_INPUT = {
     name: '',
@@ -71,106 +74,112 @@ const ServicePopup = ({
   };
 
   return (
-    <div className='absolute bg-zinc-600 w-[600px] left-[calc(50%-300px)] px-12 pb-16 pt-4 rounded mt-[50px]'>
-      <div className='flex justify-end mb-12 -mr-8'>
-        <img
-          className='w-4 cursor-pointer'
-          src={close}
-          alt='close'
-          onClick={closePopup}
-        />
-      </div>
-      <div className='px-2 mb-4 flex justify-between'>
-        <h2 className='text-xl text-orange-400 font-bold'>
-          {isExistent ? 'View Service' : 'New Service'}
-        </h2>
-        {isExistent && !editMode && (
+    <div>
+      <div
+        className='absolute w-[calc(100%)] h-[calc(100%+6rem)] -mt-[6rem] bg-zinc-300/40'
+        onClick={closePopup}
+      />
+      <div className='absolute bg-zinc-600 w-[600px] left-[calc(50%-300px)] px-12 pb-16 pt-4 rounded mt-[3rem] z-30'>
+        <div className='flex justify-end mb-12 -mr-8'>
           <img
-            className='w-5 cursor-pointer'
-            src={edit}
-            alt='edit'
-            onClick={() => setEditMode(true)}
-          />
-        )}
-      </div>
-      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
-        <ServiceInput
-          type='text'
-          name='name'
-          placeholder='Service name'
-          value={name}
-          handleChange={handleChange}
-          required
-        />
-        <div className='flex gap-3'>
-          <ServiceInput
-            type='number'
-            name='mileage'
-            placeholder='Mileage'
-            value={mileage}
-            handleChange={handleChange}
-            required
-          />
-          <ServiceInput
-            type='date'
-            name='serviceDate'
-            value={serviceDate}
-            handleChange={handleChange}
-            required
+            className='w-4 cursor-pointer'
+            src={close}
+            alt='close'
+            onClick={closePopup}
           />
         </div>
-        <textarea
-          className='bg-zinc-300 rounded-sm resize-y focus:outline-none p-2'
-          name='note'
-          placeholder='Note'
-          value={note}
-          onChange={handleChange}
-          rows={4}
-        />
-        <div className='flex gap-3'>
+        <div className='px-2 mb-4 flex justify-between'>
+          <h2 className='text-xl text-orange-400 font-bold'>
+            {isExistent ? 'View Service' : 'New Service'}
+          </h2>
+          {isExistent && !editMode && (
+            <img
+              className='w-5 cursor-pointer'
+              src={edit}
+              alt='edit'
+              onClick={() => setEditMode(true)}
+            />
+          )}
+        </div>
+        <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
           <ServiceInput
-            type='number'
-            name='price'
-            placeholder='Price'
-            value={price}
+            type='text'
+            name='name'
+            placeholder='Service name'
+            value={name}
             handleChange={handleChange}
             required
           />
-          <div className='w-full h-8 bg-zinc-300 flex rounded-sm'>
-            <div className='flex gap-4'>
-              <label className='flex items-center gap-1'>
-                <span className='px-2'>Warranty </span>
-                <input
-                  type='checkbox'
-                  name='isWarranty'
-                  checked={isWarranty}
-                  onChange={handleChange}
-                />
-              </label>
+          <div className='flex gap-3'>
+            <ServiceInput
+              type='number'
+              name='mileage'
+              placeholder='Mileage'
+              value={mileage}
+              handleChange={handleChange}
+              required
+            />
+            <ServiceInput
+              type='date'
+              name='serviceDate'
+              value={serviceDate}
+              handleChange={handleChange}
+              required
+            />
+          </div>
+          <textarea
+            className='bg-zinc-300 rounded-sm resize-y focus:outline-none p-2'
+            name='note'
+            placeholder='Note'
+            value={note}
+            onChange={handleChange}
+            rows={4}
+          />
+          <div className='flex gap-3'>
+            <ServiceInput
+              type='number'
+              name='price'
+              placeholder='Price'
+              value={price}
+              handleChange={handleChange}
+              required
+            />
+            <div className='w-full h-8 bg-zinc-300 flex rounded-sm'>
+              <div className='flex gap-4'>
+                <label className='flex items-center gap-1'>
+                  <span className='px-2'>Warranty </span>
+                  <input
+                    type='checkbox'
+                    name='isWarranty'
+                    checked={isWarranty}
+                    onChange={handleChange}
+                  />
+                </label>
+              </div>
             </div>
           </div>
-        </div>
-        <div className='w-3/5 m-auto'>
-          {(!isExistent || editMode) && (
-            <CustomButton type='submit' btnStyle='p-1 my-4 w-full'>
-              Save
-            </CustomButton>
-          )}
-          {isExistent && editMode && (
-            <div className='flex flex-col justify-center'>
-              <span
-                className='m-auto border-b border-amber-500 text-amber-500 font-bold text-sm cursor-pointer my-4'
-                onClick={cancelEditMode}
-              >
-                Cancel Edit
-              </span>
-              <span className='m-auto border-b border-red-500 text-red-500 font-bold text-sm cursor-pointer'>
-                Delete Service
-              </span>
-            </div>
-          )}
-        </div>
-      </form>
+          <div className='w-3/5 m-auto'>
+            {(!isExistent || editMode) && (
+              <CustomButton type='submit' btnStyle='p-1 my-4 w-full'>
+                Save
+              </CustomButton>
+            )}
+            {isExistent && editMode && (
+              <div className='flex flex-col justify-center'>
+                <span
+                  className='m-auto border-b border-amber-500 text-amber-500 font-bold text-sm cursor-pointer my-4'
+                  onClick={cancelEditMode}
+                >
+                  Cancel Edit
+                </span>
+                <span className='m-auto border-b border-red-500 text-red-500 font-bold text-sm cursor-pointer'>
+                  Delete Service
+                </span>
+              </div>
+            )}
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
