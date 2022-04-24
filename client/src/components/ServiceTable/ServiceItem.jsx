7d3d67c98f5a7ffe69706a3337214dce7fd70bc1
jeff <jeff.kim713@gmail.com>
2022-04-24 02:26:08 -0700

    update style on service tabla and item

--- client/src/components/ServiceTable/ServiceItem.jsx
+++ client/src/components/ServiceTable/ServiceItem.jsx
@@ -18,15 +18,15 @@ const ServiceItem = ({
   };
   return (
     <div
-      className={`w-full grid grid-cols-6 gap-2 items-stretch text-center py-1 mt-0.5 cursor-pointer ${
+      className={`w-full grid grid-cols-6 gap-1 items-stretch text-center py-1 px-1 mt-0.5 cursor-pointer ${
         index % 2 ? 'bg-neutral-600' : 'bg-neutral-500'
       }`}
       onClick={openServicePopup}
     >
-      <div className='col-span-2 overflow-hidden'>{name}</div>
-      <div>{mileage}</div>
-      <div>{isWarranty ? 'Yes' : 'No'}</div>
-      <div className='col-span-2'>{serviceDate}</div>
+      <p className='col-span-2 overflow-hidden whitespace-nowrap'>{name}</p>
+      <p>{mileage}</p>
+      <p>{isWarranty ? 'Yes' : 'No'}</p>
+      <p className='col-span-2'>{serviceDate}</p>
     </div>
   );
 };
--- client/src/components/ServiceTable/ServiceTable.jsx
+++ client/src/components/ServiceTable/ServiceTable.jsx
@@ -1,13 +1,18 @@
 import { ServiceItem, NoService } from '.';
 
-const ServiceTable = ({ services, popupDisplay, setPopupDisplay, setServiceToDisplay }) => {
+const ServiceTable = ({
+  services,
+  popupDisplay,
+  setPopupDisplay,
+  setServiceToDisplay,
+}) => {
   return (
     <div className='font-semibold flex flex-col items-center w-full pt-2 mb-4'>
-      <div className='w-full bg-amber-600 grid grid-cols-6 gap-2  py-1 items-stretch text-center'>
-        <div className='col-span-2'>Service</div>
-        <div>Mileage</div>
-        <div>Warranty</div>
-        <div className='col-span-2'>Date</div>
+      <div className='w-full bg-amber-600 grid grid-cols-6 gap-1 py-1 items-stretch text-center px-1'>
+        <p className='col-span-2'>Service</p>
+        <p>Mileage</p>
+        <p>Warranty</p>
+        <p className='col-span-2'>Date</p>
       </div>
       {services.length > 0 ? (
         services.map((service, index) => (
