1188cea998f99f9a3c6709e08a0c64e6db8acf66
jeff <jeff.kim713@gmail.com>
2022-04-27 20:50:44 -0700

    implement button to bring register component

--- client/src/components/Pages/Home/HomePage.jsx
+++ client/src/components/Pages/Home/HomePage.jsx
@@ -1,12 +1,21 @@
+import { useState } from 'react';
+
 import { HomeTitle, SignUpLink } from '.';
 import { SignInForm } from '../../SigninForm';
 
 const HomePage = () => {
+  const [isToRegister, setIsToRegister] = useState(false);
   return (
     <div className='flex flex-col justify-center items-center text-slate-100 pt-[25vh]'>
       <HomeTitle />
-      <SignInForm />
-      <SignUpLink />
+      {!isToRegister ? (
+        <>
+          <SignInForm />
+          <SignUpLink toRegister={() => setIsToRegister(true)} />
+        </>
+      ) : (
+        <div>register</div>
+      )}
     </div>
   );
 };
--- client/src/components/Pages/Home/SignUpLink.jsx
+++ client/src/components/Pages/Home/SignUpLink.jsx
@@ -1,8 +1,13 @@
-const SignUpLink = () => {
+const SignUpLink = ({ toRegister }) => {
   return (
     <span className='mt-2'>
       Not registered yet? Click{' '}
-      <code className='cursor-pointer text-amber-500 font-semibold'>HERE</code>
+      <code
+        className='cursor-pointer text-amber-500 font-semibold'
+        onClick={toRegister}
+      >
+        HERE
+      </code>
     </span>
   );
 };
