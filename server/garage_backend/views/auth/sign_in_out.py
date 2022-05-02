f92c0fd30e2cc7d8a8e6ed18a08e8456fe40bdd7
jeff <jeff.kim713@gmail.com>
2022-05-02 14:18:42 -0700

    token is also sent to client upon sign in

--- server/garage_backend/views/auth/sign_in_out.py
+++ server/garage_backend/views/auth/sign_in_out.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from rest_framework.views import APIView
+from rest_framework.authtoken.models import Token
 
 from garage_backend import serializers
 from garage_backend.views.view_utils import object_utils
@@ -26,9 +27,11 @@ class SignIn(APIView):
                 return JsonResponse({"error": ["User credentials incorrect"]}, status=400)
             
             serializer = serializers.UserSerializer(user)
-            request.session['user_id'] = serializer.data["id"]
-
-            return JsonResponse(serializer.data)
+            token = Token.objects.get(user=user).key
+            
+            userData = {**serializer.data, "token": token}
+            return JsonResponse(userData) 
+        
         except Exception as e:
             print(e)
             return JsonResponse({"error": ["Server error has occurred"]}, status=500)
